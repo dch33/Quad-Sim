52ccd8ce679b1ca86d417688e85d97a194b4efbf
Jef Packer <jbpacker@ucdavis.edu>
2016-05-09 20:09:50 -0700

    untested camera added. 90% change that it doesn't work right

---
+++ Quadcopter Dynamic Modeling and Simulation/addpaths.m
@@ -0,0 +1,18 @@
+% add the path of the simulation pre-reqs
+addpath(genpath('./Simulation Files'));
+addpath(genpath('./Graphical User Interfaces'));
+
+% load current human path command
+load('./Simulation Files/Human Path Command/humanPath.mat');
+
+% load ICs
+load('./Simulation Files/Initial Conditions/Hover.mat');
+
+% load QC path command (won't be needed soon)
+load('./Simulation Files/Path Command Files/Path_Triangle.mat');
+
+% load QC structure
+load('./Simulation Files/Quadcopter Structure Files/quadModel_+.mat');
+
+% open my current simulink model
+open_system('./Simulation Files/Block Library/humanMovementTest.slx');
